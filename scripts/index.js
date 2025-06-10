70ca3812e1ea8ec7b0376af1c5a0ce9e3cf2ffb2
doneldou <doneldou@gmail.com>
2025-06-10 21:55:40 +0300

    finish work

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
+import initialCards from "./cards.js";
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardsList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(name, link, removeCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__title").textContent = name;
+  cardElement.querySelector(".card__image").src = link;
+
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", removeCard);
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function removeCard(evt) {
+  const eventTargetCard = evt.target.closest(".card");
+  eventTargetCard.remove();
+}
 
 // @todo: Вывести карточки на страницу
+for (let i = 0; i < initialCards.length; i++) {
+  cardsList.append(
+    createCard(initialCards[i].name, initialCards[i].link, removeCard)
+  );
+}
