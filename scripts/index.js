f7ca26d2c839ff3ac47478f214b2d6d29c655205
doneldou <doneldou@gmail.com>
2025-06-11 08:28:01 +0300

    corrected comments

--- scripts/index.js
+++ scripts/index.js
@@ -6,27 +6,28 @@ const cardTemplate = document.querySelector("#card-template").content;
 const cardsList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-function createCard(name, link, removeCard) {
+function createCard(cardData, removeCard) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  cardElement.querySelector(".card__title").textContent = name;
-  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(
+    ".card__image"
+  ).alt = `Изображение: ${cardData.name}`;
 
-  cardElement
-    .querySelector(".card__delete-button")
-    .addEventListener("click", removeCard);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => removeCard(cardElement));
 
   return cardElement;
 }
 
 // @todo: Функция удаления карточки
-function removeCard(evt) {
-  const eventTargetCard = evt.target.closest(".card");
-  eventTargetCard.remove();
+function removeCard(cardElement) {
+  // const eventTargetCard = evt.target.closest(".card");
+  // eventTargetCard.remove();
+  cardElement.remove();
 }
 
 // @todo: Вывести карточки на страницу
-for (let i = 0; i < initialCards.length; i++) {
-  cardsList.append(
-    createCard(initialCards[i].name, initialCards[i].link, removeCard)
-  );
-}
+initialCards.forEach((item) => {
+  cardsList.append(createCard(item, removeCard));
+});
